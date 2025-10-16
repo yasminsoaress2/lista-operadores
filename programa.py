c7d4a14bed1cd646ec098e0063edde8ee31d229b
Yasmin Soares <yasminsoaresbr123@gmail.com>
2025-10-15 21:54:37 -0300

    Atualiza o programa para solicitar dois números e exibir resultados de operações relacionais entre eles.

--- programa.py
+++ programa.py
@@ -1,4 +1,23 @@
-num1 = int(input("Digite um número: "))
-num2 = int(input("Digite outro número: "))
-soma = num1 + num2
-print(f"A soma dos dois números é: {soma}")
+#Lista de Exercícios - Operadores 
+#Nome: Yasmin Soares
+
+#Questão 1:
+#Fazer um programa que solicite ao usuário dois números e mostre todos os resultados de possíveis de operações relacionais entre eles.
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+
+print("Comparando a com b")
+print("O valor da avaliação da expressão", a, ">", b, "é", a > b)
+print("O valor da avaliação da expressão", a, "<", b, "é", a < b)
+print("O valor da avaliação da expressão", a, ">=", b, "é", a >= b)
+print("O valor da avaliação da expressão", a, "<=", b, "é", a <= b)
+print("O valor da avaliação da expressão", a, "==", b, "é", a == b)
+print("O valor da avaliação da expressão", a, "!=", b, "é", a != b)
+
+print("Comparando b com a")
+print("O valor da avaliação da expressão", b, ">", a, "é", b > a)
+print("O valor da avaliação da expressão", b, "<", a, "é", b < a)
+print("O valor da avaliação da expressão", b, ">=", a, "é", b >= a)
+print("O valor da avaliação da expressão", b, "<=", a, "é", b <= a)
+print("O valor da avaliação da expressão", b, "==", a, "é", b == a)
+print("O valor da avaliação da expressão", b, "!=", a, "é", b != a)
